f0ac500734f757620ab5b69eeafc92554082ac3f
solozobov <Andrei.Solozobov@gmail.com>
2019-10-24 01:49:57 +0300

    - update.sh corrections

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ chmod 700 ~/remember/update.sh
 mvn clean install -Dmaven.test.skip=true -Dremember.db.url=jdbc:h2:~/remember/db -Dremember.db.user=remember -Dremember.db.password=remember
 for pid in `cat ~/remember/pid`;
 do
-  kill -9 $pid;
+  kill -9 $pid || echo;
 done
 rm ~/remember/pid
 nohup /usr/bin/java -jar target/remember-1.0-SNAPSHOT.jar com.solozobov.andrei.Application --spring.config.location=file:/root/remember/application.properties 2>&1 >> ~/remember/log &
