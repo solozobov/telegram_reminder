04e6e43905ab76767c9aa69f910e27d70e14fafd
solozobov <Andrei.Solozobov@gmail.com>
2019-10-24 01:47:01 +0300

    - install.sh, update.sh corrections

--- update.sh
+++ update.sh
@@ -6,10 +6,10 @@ git pull
 cp update.sh ~/remember/update.sh
 chmod 700 ~/remember/update.sh
 mvn clean install -Dmaven.test.skip=true -Dremember.db.url=jdbc:h2:~/remember/db -Dremember.db.user=remember -Dremember.db.password=remember
-for pid in `cat pid`;
+for pid in `cat ~/remember/pid`;
 do
   kill -9 $pid;
 done
 rm ~/remember/pid
-bash /usr/bin/java -jar target/remember-1.0-SNAPSHOT.jar com.solozobov.andrei.Application --spring.config.location=classpath:~/remember/application.properties >> ~/remember/log &
+nohup /usr/bin/java -jar target/remember-1.0-SNAPSHOT.jar com.solozobov.andrei.Application --spring.config.location=file:/root/remember/application.properties 2>&1 >> ~/remember/log &
 echo $! >> ~/remember/pid
