4c8cfc80ea4c4c3091fde415dce890064d6ba6cf
solozobov <solozobov>
2022-03-29 09:52:09 +0000

    fix update.sh to work relatively from current dir

--- update.sh
+++ update.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-cd ~/telegram_reminder
+work_dir=`pwd`
+cd telegram_reminder
 git pull
-mvn clean install -Dmaven.test.skip=true -Dremember.db.url=jdbc:h2:~/remember/db -Dremember.db.user=remember -Dremember.db.password=remember
+mvn clean install -Dmaven.test.skip=true -Dremember.db.url=jdbc:h2:$work_dir/db -Dremember.db.user=remember -Dremember.db.password=remember
 
-cd ~/remember
-cp ~/telegram_reminder/update.sh update.sh
+cd ..
+cp telegram_reminder/update.sh update.sh
 chmod 700 update.sh
 for pid in `cat pid`;
 do
@@ -14,5 +15,5 @@ do
 done
 rm pid || echo
 rm db.lock.db || echo
-nohup /usr/bin/java -jar ~/telegram_reminder/target/remember-1.0-SNAPSHOT.jar com.solozobov.andrei.Application --spring.config.location=file:/root/remember/application.properties &
+nohup /usr/bin/java -jar $work_dir/remember/telegram_reminder/target/remember-1.0-SNAPSHOT.jar com.solozobov.andrei.Application --spring.config.location=file:$work_dir/remember/application.properties &
 echo $! >> pid
